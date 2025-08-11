cc050d3607cd22f65034972e6ca357432e5538da
Binod Chaudhary <thebinod7@gmail.com>
2025-08-11 20:00:32 +0545

    updated env variables

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,20 +1,23 @@
 'use client';
 
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
 import AppWrapper from './AppWrapper';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { PUBLIC_ENV } from '@/utils/env';
 
-const GOOGLE_CLIENT_ID = PUBLIC_ENV.GOOGLE_OAUTH_WEB_CLIENT_ID!;
-console.log('GOOGLE_CLIENT_ID===>', GOOGLE_CLIENT_ID);
+console.log(
+    'Process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID',
+    process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID
+);
 
 const Providers = ({ children }: { readonly children: React.ReactNode }) => {
     const [queryClient] = useState(() => new QueryClient());
 
     return (
-        <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+        <GoogleOAuthProvider
+            clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}
+        >
             <QueryClientProvider client={queryClient}>
                 <AppWrapper>{children}</AppWrapper>
                 <ReactQueryDevtools initialIsOpen={false} />
